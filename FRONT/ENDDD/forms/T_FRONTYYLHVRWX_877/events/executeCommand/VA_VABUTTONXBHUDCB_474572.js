f801d6a69f0dd0323e1130c85f61e4908037f17a
Douglas Gallardo <douglasgallardoz11@gmail.com>
2021-04-22 14:18:59 -0500

    Agregados eventos de cliente y servidor

---
+++ FRONT/ENDDD/forms/T_FRONTYYLHVRWX_877/events/executeCommand/VA_VABUTTONXBHUDCB_474572.js
@@ -0,0 +1,11 @@
+
+
+//Entity: EstudianteTODOS
+//EstudianteTODOS. (Button) View: EstudianteDGZForm
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONXBHUDCB_474572 = function(  entities, executeCommandEventArgs ) {
+
+    executeCommandEventArgs.commons.execServer = true;
+    //executeCommandEventArgs.commons.serverParameters.EstudianteTODOS = true;
+
+};
