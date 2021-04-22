990368b7e27ee9f1c02a867589d0af1f532d8ff4
kary17 <vero_kary1995@hotmail.com>
2021-04-22 17:18:56 -0500

    Funcionamiento boton guardar

--- FRONT/ENDDD/forms/T_FRONTRPGDCKMI_937/events/executeCommand/VA_VABUTTONIGJPMOU_866938.js
+++ FRONT/ENDDD/forms/T_FRONTRPGDCKMI_937/events/executeCommand/VA_VABUTTONIGJPMOU_866938.js
@@ -7,5 +7,14 @@ task.executeCommand.VA_VABUTTONIGJPMOU_866938 = function(  entities, executeComm
 
     executeCommandEventArgs.commons.execServer = true;
     //executeCommandEventArgs.commons.serverParameters.EstudianteTODOS = true;
+    let nom = entities.EstudianteTODOS.nombre;
+    let ap = entities.EstudianteTODOS.apellido;
+    let edad = entities.EstudianteTODOS.edad;
+    let sexo = entities.EstudianteTODOS.sexo;
+    
+    console.log("nombre: "+nom);
+    console.log("apellido: "+ap);
+    console.log("edad: "+edad);
+    console.log("sexo: "+sexo);
 
 };
