21de8390543505ddedad4eb20f5d18126064fe47
Alberto Carrera <acarrera_l@hotmail.com>
2021-04-22 17:44:36 -0500

    Cambio botón AFCLForm

---
+++ FRONT/ENDDD/forms/T_FRONTUVXMNZNU_590/events/executeCommand/VA_VABUTTONMRSTWDT_943602.js
@@ -0,0 +1,11 @@
+
+
+//Entity: EstudianteTODOS
+//EstudianteTODOS. (Button) View: EstudianteAFCLForm
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONMRSTWDT_943602 = function(  entities, executeCommandEventArgs ) {
+
+    executeCommandEventArgs.commons.execServer = true;
+    //executeCommandEventArgs.commons.serverParameters.EstudianteTODOS = true;
+
+};
