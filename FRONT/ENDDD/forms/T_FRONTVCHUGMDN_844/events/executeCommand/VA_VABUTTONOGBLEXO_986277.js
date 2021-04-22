0a5da05886eda8140a8b75013bb34319ae0284e6
Olmer Fernando Velasquez <fernandopsmart@gmail.com>
2021-04-22 14:26:47 -0500

    se agrega codigo java script

--- FRONT/ENDDD/forms/T_FRONTVCHUGMDN_844/events/executeCommand/VA_VABUTTONOGBLEXO_986277.js
+++ FRONT/ENDDD/forms/T_FRONTVCHUGMDN_844/events/executeCommand/VA_VABUTTONOGBLEXO_986277.js
@@ -5,7 +5,9 @@
 //Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
 task.executeCommand.VA_VABUTTONOGBLEXO_986277 = function(  entities, executeCommandEventArgs ) {
 
+    const Estudiante = entities.EstudianteTODOS;
+    console.log("Nombre: " + Estudiante.nombre + " ,Apellido: "+Estudiante.apellido);
+    
     executeCommandEventArgs.commons.execServer = true;
-    //executeCommandEventArgs.commons.serverParameters.EstudianteTODOS = true;
-
+    
 };
