0cbedb5a25666814cc5a9acfeae401f09a8666d7
domorales <domorale@espol.edu.ec>
2021-04-22 13:32:30 -0500

    Se añadio el formulario EstudianteDmpForm

---
+++ FRONT/ENDDD/forms/T_FRONTJXBJZVQW_794/events/executeCommand/VA_VABUTTONATIKKWA_932133.js
@@ -0,0 +1,11 @@
+
+
+//Entity: EstudianteTODOS
+//EstudianteTODOS. (Button) View: EstudianteDmpForm
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONATIKKWA_932133 = function(  entities, executeCommandEventArgs ) {
+
+    executeCommandEventArgs.commons.execServer = true;
+    //executeCommandEventArgs.commons.serverParameters.EstudianteTODOS = true;
+
+};
