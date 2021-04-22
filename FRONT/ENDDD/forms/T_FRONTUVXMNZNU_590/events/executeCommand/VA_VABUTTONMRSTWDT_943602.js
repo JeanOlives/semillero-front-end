6d12155eae1eb3606db36243e682b03d24b7f594
Alberto Carrera <acarrera_l@hotmail.com>
2021-04-22 17:51:14 -0500

    Añado evento en cliente AFCLForm

--- FRONT/ENDDD/forms/T_FRONTUVXMNZNU_590/events/executeCommand/VA_VABUTTONMRSTWDT_943602.js
+++ FRONT/ENDDD/forms/T_FRONTUVXMNZNU_590/events/executeCommand/VA_VABUTTONMRSTWDT_943602.js
@@ -4,7 +4,8 @@
 //EstudianteTODOS. (Button) View: EstudianteAFCLForm
 //Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
 task.executeCommand.VA_VABUTTONMRSTWDT_943602 = function(  entities, executeCommandEventArgs ) {
-
+    let nombreCompleto = entities.EstudianteTODOS.nombre+" "+entities.EstudianteTODOS.apellido;
+    console.log(nombreCompleto);
     executeCommandEventArgs.commons.execServer = true;
     //executeCommandEventArgs.commons.serverParameters.EstudianteTODOS = true;
 
