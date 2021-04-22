5831ae61ad99de9491a2b96574343d25ad9dd897
Jean <jean.olives@pucese.edu.ec>
2021-04-22 12:19:01 -0500

    version nueva y limpia del proyecto

---
+++ FRONT/ENDDD/forms/T_FRONTGLOZBOPH_997/events/executeCommand/VA_VABUTTONAMXPFRM_734708.js
@@ -0,0 +1,11 @@
+
+
+//Entity: EstudianteTODOS
+//EstudianteTODOS. (Button) View: EstudianteJcoForm
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONAMXPFRM_734708 = function(  entities, executeCommandEventArgs ) {
+
+    executeCommandEventArgs.commons.execServer = true;
+    //executeCommandEventArgs.commons.serverParameters.EstudianteTODOS = true;
+
+};
