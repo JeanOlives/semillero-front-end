5d9e5d54e0e32c9028a16deeaaa2f8c3763c126b
Olmer Fernando Velasquez <fernandopsmart@gmail.com>
2021-04-22 13:56:46 -0500

    se agrega formulario OFV

---
+++ FRONT/ENDDD/forms/T_FRONTVCHUGMDN_844/events/executeCommand/VA_VABUTTONOGBLEXO_986277.js
@@ -0,0 +1,11 @@
+
+
+//Entity: EstudianteTODOS
+//EstudianteTODOS. (Button) View: EstudianteOFVA
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONOGBLEXO_986277 = function(  entities, executeCommandEventArgs ) {
+
+    executeCommandEventArgs.commons.execServer = true;
+    //executeCommandEventArgs.commons.serverParameters.EstudianteTODOS = true;
+
+};
