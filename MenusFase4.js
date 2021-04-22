9e0f133f950fe35c5c09c20043048b9d10ec4a76
kary17 <vero_kary1995@hotmail.com>
2021-04-22 18:52:45 -0500

    Se agrega etiqueta menu Estudiante VPI

--- MenusFase4.js
+++ MenusFase4.js
@@ -4,7 +4,8 @@
 			
 			"MNU_FASE4": "Fase 4",
 			"Grupo A": "Grupo A",
-			"MNU_JCGR_CAP_SEM4": "Estudiante JCGR"
+			"MNU_JCGR_CAP_SEM4": "Estudiante JCGR",
+			"MNU_TALLER_VPI":"Estudiante VPI"
         }
     }
 }
