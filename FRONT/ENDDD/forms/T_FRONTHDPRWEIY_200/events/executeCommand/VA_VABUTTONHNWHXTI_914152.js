90b6d96257b849d0dab8fb41a7089f36ecbb2ba0
Bruno Dueñas <bruno.duenas@udla.edu.ec>
2021-04-22 14:17:12 -0500

    Eventos Cliente Servidor BDF

---
+++ FRONT/ENDDD/forms/T_FRONTHDPRWEIY_200/events/executeCommand/VA_VABUTTONHNWHXTI_914152.js
@@ -0,0 +1,11 @@
+
+
+//Entity: EstudianteTODOS
+//EstudianteTODOS. (Button) View: EstudianteBDFForm
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONHNWHXTI_914152 = function(  entities, executeCommandEventArgs ) {
+
+    executeCommandEventArgs.commons.execServer = true;
+    //executeCommandEventArgs.commons.serverParameters.EstudianteTODOS = true;
+
+};
