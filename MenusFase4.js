d618095b9d104750d8167398ecbb8ed50e031f03
afcarrera <33518086+afcarrera@users.noreply.github.com>
2021-04-22 19:31:01 -0500

    Update MenusFase4.js

--- MenusFase4.js
+++ MenusFase4.js
@@ -5,7 +5,8 @@
 			"MNU_FASE4": "Fase 4",
 			"Grupo A": "Grupo A",
 			"MNU_JCGR_CAP_SEM4": "Estudiante JCGR",
-			"MNU_TALLER_VPI":"Estudiante VPI"
+			"MNU_TALLER_VPI":"Estudiante VPI",
+			"MNU_ACA_TALLER_2":"Estudiante AFCL"
         }
     }
 }
